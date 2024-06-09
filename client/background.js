a6c34d7dc61b8a177bc33bef62588adc2d293715
vishnuoruphones <vswaroop04@gmail.com>
2024-06-10 00:05:06 +0530

    feat : added options, background script, alarm and notifications

---
+++ client/background.js
@@ -0,0 +1,18 @@
+console.log("Hi from background script");
+
+chrome.alarms.create({
+  periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    chrome.storage.local.set({
+      timer: time + 1,
+    });
+    // this.registration.showNotification("Chrome Timer Extension", {
+    //   body: "1 second has passed",
+    //   icon: "icon.png",
+    // });
+  });
+});
