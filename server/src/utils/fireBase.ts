0ebb1b9c5435796005d4f5fdfe88e16feee73af4
vishnuoruphones <vswaroop04@gmail.com>
2024-06-17 00:34:42 +0530

    feat : firebase it + ui comp

---
+++ server/src/utils/fireBase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: 'github-chrome-extension.firebaseapp.com',
+	projectId: 'github-chrome-extension',
+	storageBucket: 'github-chrome-extension.appspot.com',
+	messagingSenderId: '1084770993403',
+	appId: '1:1084770993403:web:c767873b314c00b3dcdc5e',
+	measurementId: 'G-WFFEVG7TE2',
+};
