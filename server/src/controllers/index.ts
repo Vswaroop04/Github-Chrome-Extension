7c368296f19ccee039b31d1b57b4022c8417c5ea
vishnuoruphones <vswaroop04@gmail.com>
2024-07-11 19:17:44 +0530

    test : testing commit 2

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const index = (req: Request, res: Response) => {
 	res.status(200).json({
-		message: "Vishnu's Github Chrome Extension Backend is Running :>>>>>>>>>>>>>",
+		message: "Vishnu's Github Chrome Extension Backend is Running :>",
 	});
 };
 
